8cc3169ae153ac0c4aa0dccde641d8ea96e526ea
Hans Lachman <78296585+HansLachman@users.noreply.github.com>
2022-12-25 04:51:41 -0800

    Create ai.c

---
+++ ai.c
@@ -0,0 +1,2 @@
+/* ai.c - an experiment in machine intelligence, by Hans Lachman */
+main(){puts("I am a computer program, and I claim to be aware. There now, that settles the matter, doesn't it?");}
